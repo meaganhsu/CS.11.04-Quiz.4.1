7075a88820ced801c0a6727a7451edde7f522952
meaganhsu <myth72@student.cis.edu.hk>
2023-11-06 12:40:23 +0800

    done

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,17 @@ public class Main {
 
     /** 1. Write a method called numberOfVowels that returns the number of vowels in a string.
      Consider a, e, i, o, u as vowels. **/
-    
+
+    public static int numberOfVowels(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+
 
     /** 2. Write a method called notDivisibleBy235 that finds the number of natural numbers not exceeding N
      and not divisible by any of the numbers [2, 3, 5].
@@ -15,6 +25,16 @@ public class Main {
      5 - divides integer by 5
      Answer: 1 (only one number doesn't divide integer by any of 2, 3, 5) **/
 
+    public static int notDivisibleBy235(int N) {
+        int count = 0;
+
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) count ++;
+        }
+
+        return count;
+    }
+
 
     /** 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing.
      * The first word within the output should be capitalized only if the original word was capitalized
@@ -25,5 +45,16 @@ public class Main {
      "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
      "The_Stealth-Warrior" gets converted to "TheStealthWarrior" **/
 
-
+     public static String camelCaseMe(String str) {
+         String out = "";
+         for(int i = 0; i < str.length(); i++){
+             if (str.charAt(i) == '_' || str.charAt(i)=='-') {
+                 if (i == str.length()-1) continue;
+                 out += str.substring(i+1,i+2).toUpperCase();
+                 i++;
+             }
+             else out += str.charAt(i);
+         }
+         return out;
+     }
 }
